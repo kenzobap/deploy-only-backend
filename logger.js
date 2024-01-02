7a2e029b53649086c3311c794bf303dcd570c2f5
kenzobap <overdev.khang@gmail.com>
2024-01-03 03:07:06 +0700

    edit logger

--- logger.js
+++ logger.js
@@ -1,35 +1,40 @@
-import path from 'path'
-import winston from 'winston'
-import { createLogger, format, transports } from 'winston'
-const filename = path.join(__dirname, 'create-file-logs.log')
+const path = require('path');
+const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+
+const filename = path.join(__dirname, 'created-logfile.log');
 
 const logger = winston.createLogger({
-    // transports:
-    //     [
-    //         new winston.transports.Console()
-    //     ],
-    transports:
-        [
-            new winston.transports.Console(),
-            new winston.transports.File({ filename })
-        ],
+    //using console
+    transports: [
+        new winston.transports.Console(),
+    ],
+
+
+    //create file logs
+    // transports: [
+    //     new winston.transports.Console(),
+    //     new winston.transports.File({ filename })
+    // ],
+
     //format timestamp
-    format: format.combine(
-        format.label({ label: '[my-service]' }),
-        format.timestamp({
-            format: 'DD-MM-YYYY HH:mm:ss'
-        }),
-        //
-        // The simple format outputs
-        // `${level}: ${message} ${[Object with everything else]}`
-        //
-        format.simple()
-        //
-        // Alternatively you could use this custom printf format if you
-        // want to control where the timestamp comes in your final message.
-        // Try replacing `format.simple()` above with this:
-        //
-        // format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-    )
-})
-export default logger
+    // format: format.combine(
+    //     format.label({ label: '[my-service]' }),
+    //     format.timestamp({
+    //         format: 'DD-MM-YYYY HH:mm:ss'
+    //     }),
+    //     //
+    //     // The simple format outputs
+    //     // `${level}: ${message} ${[Object with everything else]}`
+    //     //
+    //     format.simple()
+    //     //
+    //     // Alternatively you could use this custom printf format if you
+    //     // want to control where the timestamp comes in your final message.
+    //     // Try replacing `format.simple()` above with this:
+    //     //
+    //     // format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    // )
+});
+
+module.exports = logger
